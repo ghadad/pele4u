af0a944c0690054126be16a9f63c92996e10128c
golanh <golanh@localhost.localdomain>
2020-02-23 16:29:37 +0200

    wip

--- www/app/directives.js
+++ www/app/directives.js
@@ -438,4 +438,14 @@ angular.module('tabSlideBox', [])
         ngModelCtrl.$parsers.length = 0;
       }
     };
-  });
+  }).directive('focusMe', function($timeout) {
+    return {
+      link: function(scope, element, attrs) {
+  
+        $timeout(function() {
+          element[0].focus(); 
+        });
+      }
+    };
+  }); 
+  
