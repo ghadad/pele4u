22aa95b2ae7dff7421cc2403159e591a1642da27
Golan Hadad <ghadad@gmail.com>
2020-06-14 15:19:00 +0300

    wip

--- www/app/factories.js
+++ www/app/factories.js
@@ -1559,7 +1559,7 @@ angular.module('pele.factories', ['ngStorage', 'LocalStorageModule', 'ngCordova'
                      if (!result.nativeURL) {
                       self.hideLoading();
                     } else {
-                      cordova.plugins.fileOpener2.open(result.nativeURL, "text/vcard", function(){},function(){});
+                      cordova.plugins.fileOpener2.open(result.nativeURL, "application/vcard+xml", function(){},function(){});
                      }
               },function (error) {
                 self.hideLoading()
