5bcfc44f7f1d7f2a8baa68bbd87c9600b56bbfe5
Golan Hadad <ghadad@gmail.com>
2020-06-15 14:45:26 +0300

    wip

--- www/app/GetUserMenu.js
+++ www/app/GetUserMenu.js
@@ -438,9 +438,7 @@ app.controller('GetUserMenuCtrl',
         TOKEN: $sessionStorage.AuthInfo.token
       };
       if (appConfig.ApplicationType === "EXT") {
-        var win = window.open(appConfig.Path, '_system','location=yes');
-        win.onerror = function() {  };
-
+        return window.open(appConfig.Path, '_system','location=yes');
       } else if (appConfig.ApplicationType === "IAB") {
         return ApiGateway.openInApp(appConfig.Path);
       } else if (appConfig.ApplicationType === "IEB") {
