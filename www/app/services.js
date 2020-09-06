7ccc2e08a40ea4b027cf34663e1d9d1a9d3de4d1
ghadad <ghadad@gmail.com>
2020-09-06 23:47:41 +0300

    wip

--- www/app/services.js
+++ www/app/services.js
@@ -418,14 +418,14 @@ app.service('StorageService', ['$http', 'PelApi', '$localStorage', function ($ht
                 switch ( res ) {
                   case "error" :
                     clearInterval( loop );
-                //    inAppBrowserRef.close();
+                    inAppBrowserRef.close();
                     break ;
                     case "success" :
                     clearInterval( loop );
                     break ;
                     case "timeout" :
                     clearInterval( loop );
-                //    inAppBrowserRef.close();
+                    inAppBrowserRef.close();
                     break ;
                     case "progress":
                       break 
